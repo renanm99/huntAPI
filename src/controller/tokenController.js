81c783b287c968e53caef54f0d13e7d55988b8a4
Renan Oliveira <renan.oliveira2199@gmail.com>
2019-09-04 20:57:14 -0300

    nodemailer

--- src/controller/tokenController.js
+++ src/controller/tokenController.js
@@ -1,15 +1,39 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 const Token = mongoose.model("Token");
 
 module.exports = {
-    async validarToken(req, res) {
-        const token = await Token.findOne({
-            token: req.params.idtoken,
-            isAvailable: true
-        });
-        if(token === null){
-            res.send({token:"",email:"",isAvailable:false});
-        }
-        res.send(token);
+  async validarToken(req, res) {
+    const token = await Token.findOne({
+      token: req.params.idtoken,
+      isAvailable: true
+    });
+    if (token === null) {
+      res.send({ token: "", email: "", isAvailable: false });
+    } else {
+      res.send(token);
     }
+  },
+  async enviarToken(req, res) {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      //auth: { user: "@gmail.com", pass: "" }
+    });
+
+    var mailOptions = {
+      from: "",
+      to: "",
+      subject: "Aqui está seu email",
+      text: ""
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.send({response:"0",messageId:"0"});
+      } else {
+        res.send({response:info.response,messageId:info.messageId});
+        //salvar no banco
+      }
+    });
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const tokenController = require("./controller/tokenController");
 
 routes.post("/cadastro/usuario", usuarioController.userCreate);
 routes.get("/validar/token/:idtoken", tokenController.validarToken);
+routes.get("/teste/email",tokenController.enviarToken);
 
 module.exports = routes;
